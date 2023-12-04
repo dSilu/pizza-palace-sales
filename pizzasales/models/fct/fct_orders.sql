ff5f046cbff8f8e68cab0334142467176b295c32
dSilu <sahooashru@gmail.com>
2023-12-05 01:24:50 +0530

    Updated fct_orders.sql.

--- pizzasales/models/fct/fct_orders.sql
+++ pizzasales/models/fct/fct_orders.sql
@@ -4,10 +4,16 @@
 
 WITH stg_order_details AS (
     select * from {{ ref('stg_order_details') }}
-)
+),
+stg_orders AS (
+    SELECT * FROM {{ ref('stg_orders') }}
+) 
 
 SELECT
-    ORDER_ID,
-    SUM(ORDER_QUANITY) AS TOTAL_ORDER_QUANTITY,
-    SUM(ORDER_AMOUNT) AS TOTAL_ORDER_AMOUNT
-FROM stg_order_details
+    od.ORDER_ID,
+    o.ORDER_DATETIME,
+    SUM(od.ORDER_QUANITY) AS TOTAL_ORDER_QUANTITY,
+    SUM(od.ORDER_AMOUNT) AS TOTAL_ORDER_AMOUNT
+FROM stg_order_details od.
+LEFT JOIN stg_orders o 
+    USING (ORDER_ID)
